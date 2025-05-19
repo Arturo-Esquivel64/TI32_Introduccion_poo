d6701b1bfed0e233961996c4cca256fc1fc5a3c5
Arturo-Esquivel64 <1724110583@utectulancingo.edu.mx>
2025-05-19 20:10:25 +0000

    UPDATE se agrego comentario al codigo

--- semana3/aplicacion_02.py
+++ semana3/aplicacion_02.py
@@ -7,29 +7,33 @@ class Calculadora:
     def sumar(self, numero1, numero2):
         # Recibe 2 valores, los suma y los almacena en resultados.
         resultado = numero1 + numero2
+        # Imprime el texto Resultado y el valor de la variable resultado.
         print(f"Resultado suma: {resultado}")
 
     def resta(self, numero1, numero2):
         # Recibe 2 valores, los resta y los almacena en resultados.
         resultado = numero1 - numero2
+        # Imprime el texto Resultado y el valor de la variable resultado.
         return resultado
 
     def multiplicar(self, numero1, numero2):
         # Recibe 2 valores, los multiplica y los almacena en resultados.
         resultado = numero1 * numero2
+        # Imprime el texto Resultado y el valor de la variable resultado.
         print(f"Resultado multiplicacion: {resultado}")
 
     def dividir(self, numero1, numero2):
         # Recibe 2 valores, los divide y los almacena en resultados.
         try:
             resultado = numero1 / numero2
+            # Imprime el texto Resultado y el valor de la variable resultado.
             return resultado
         except ZeroDivisionError as error:
             print(f"Error 001: {error.args[0]}")
         except  TypeError as error:
             print(f"Error 002: {error.args[0]}")
         except Exception as error:
-            # Error que atrapa todo tipo de errores.
+            # Guarda todo tipo de errores.
             print(f"Error 003: {error.args[0]}")
 
 mi_objeto = Calculadora()
